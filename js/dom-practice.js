9fe8eda76f91ed7ed05e9e68653b871ba17dbbe1
Kamrul Hasan <you@example.com>
2022-02-07 16:44:00 +0600

    style given by using getElementsByTagName

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -1,1 +1,29 @@
-console.log(5);
+
+
+// given style in article tag by using getElementsByTagName in JS
+const articles = document.getElementsByTagName('article');
+for (const article of articles) {
+    article.style.border = '2px solid red';
+    article.style.margin = '5px';
+    article.style.padding = '5px';
+    article.style.borderRadius = '10px';
+}
+
+// style given in p tag
+const pTags = document.getElementsByTagName('p');
+for(const pTag of pTags ){
+    pTag.style.fontSize = '18px';
+    pTag.style.color = 'green';
+}
+
+// style given in h3 tag
+const h3Tags = document.getElementsByTagName('h3');
+for(const h3Tag of h3Tags){
+    h3Tag.style.textAlign = 'center';
+    h3Tag.style.backgroundColor = 'yellow';
+}
+
+//========================================================================
+
+
+
